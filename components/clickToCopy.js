665b47f75137e2fe26a63d60e4e64fb397cd8ee8
Amazingandyyy <amazingandyyy@gmail.com>
2017-05-11 01:09:21 -0700

    remove used styling

--- components/clickToCopy.js
+++ components/clickToCopy.js
@@ -29,31 +29,6 @@ export default class ClickToCopy extends React.Component {
         </span>
         {children}
         <style jsx>{`
-            .copyBtn {
-                opacity: 0.7;
-                font-family: 'Open Sans', 'Helvetica', 'Sans';
-                font-size: 0.7rem;
-                margin: auto 5px;
-                padding: 0px 5px;
-                border: 1px dotted rgba(0,0,0,0.7);
-                border-radius: 3px;
-                text-align: center;
-                cursor: pointer;
-                transition: .1s all ease-in-out;
-            }
-            .copyBtn:hover {
-                transition: 0s all;
-                opacity: 0.8;
-                border: 1px solid rgba(0,0,0,0.8);
-            }
-            .copyBtn:active {
-                opacity: 1;
-            }
-            .copyElem.true .copyBtn{
-                border: 1px solid #FD6C6C;
-                color: white;
-                background-color: #FD6C6C;
-            }
             .copyElem.true{
                 color: rgba(0,0,0,0.4);
             }
