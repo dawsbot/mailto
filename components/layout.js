4dbc7d6b9504222c1b54329f92dd5ec652e7212b
Dawson Botsford <DawsonBotsford@gmail.com>
2017-11-16 11:01:42 -0800

    remove foux

--- components/layout.js
+++ components/layout.js
@@ -9,26 +9,24 @@ export default ({ children }) => (
       </title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <style>{`
-        body {
-          margin: 80px 20px;
-          font-family: 'Coming Soon', cursive;
-        }
-      `}</style>
       <link
         href="https://fonts.googleapis.com/css?family=Coming+Soon"
         rel="stylesheet"
       />
+    </Head>
+    <div className="notbody">
       <GHCorner
         href="https://github.com/dawsbot/mailto"
         bannerColor="#FD6C6C"
         height="100px"
         width="100px"
       />
-    </Head>
-    <div className="notbody">
       {children}
-      <style jsx>{`
+      <style jsx global>{`
+        body {
+          margin: 80px 20px;
+          font-family: 'Coming Soon', cursive;
+        }
         .notbody {
           max-width: 600px;
           margin: auto;
