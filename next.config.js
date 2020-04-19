02fd28b6070296f99ac535d755a2e392b2b7c0fd
Dawson Botsford <dawsonbotsford@gmail.com>
2020-04-19 17:37:48 -0400

    more secrets config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
+  env: {
+    GA_TRACKING_ID: process.env.GA_TRACKING_ID
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Add the new plugin to the existing webpack plugins
     config.plugins.push(new Dotenv({ silent: true }));
