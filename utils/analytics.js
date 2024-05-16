9a099585ba76a060188856e4a63cfbf00c859dcd
dawsbot <dawsonbotsford@gmail.com>
2024-05-16 16:01:53 -0600

    remove unneeded logging

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,17 +1,15 @@
-/* eslint-disable */
 import ReactGA from 'react-ga';
 export const initGA = () => {
-  console.log('initGA with id ', process.env.NEXT_PUBLIC_GA_TRACKING_ID);
   ReactGA.initialize(process.env.NEXT_PUBLIC_GA_TRACKING_ID);
 };
 export const logPageView = () => {
-  console.log('logging page view');
+  // console.log('logging page view');
 
   ReactGA.set({ page: window.location.pathname });
   ReactGA.pageview(window.location.pathname);
 };
 export const logEvent = (action, mailtoHref) => {
-  console.log('logging event' + action + ' for href ' + mailtoHref);
+  // console.log('logging event' + action + ' for href ' + mailtoHref);
   if (mailtoHref) {
     ReactGA.event({ category: 'button-click', action, label: mailtoHref });
     return;
