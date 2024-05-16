a8f964d3e2cf0aef58481f02ec6193d39fb3de6f
Dawson Botsford <dawsonbotsford@gmail.com>
2024-05-16 16:08:53 -0600

    🧼 Modernize via version bumps and prettier simplicity (#43)
    * So many version updates
    
    * dev dep bumpin
    
    * remove unused env
    
    * husky and lint-staged
    
    * remove unneeded logging
    
    * make eslint work with latest jsxa11y
    
    * prettier

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,24 +1,22 @@
-/* eslint-disable */
-import ReactGA from 'react-ga';
+import ReactGA from "react-ga";
 export const initGA = () => {
-  console.log('initGA with id ', process.env.GA_TRACKING_ID);
-  ReactGA.initialize(process.env.GA_TRACKING_ID);
+  ReactGA.initialize(process.env.NEXT_PUBLIC_GA_TRACKING_ID);
 };
 export const logPageView = () => {
-  console.log('logging page view');
+  // console.log('logging page view');
 
   ReactGA.set({ page: window.location.pathname });
   ReactGA.pageview(window.location.pathname);
 };
 export const logEvent = (action, mailtoHref) => {
-  console.log('logging event' + action + ' for href ' + mailtoHref);
+  // console.log('logging event' + action + ' for href ' + mailtoHref);
   if (mailtoHref) {
-    ReactGA.event({ category: 'button-click', action, label: mailtoHref });
+    ReactGA.event({ category: "button-click", action, label: mailtoHref });
     return;
   }
-  ReactGA.event({ category: 'button-click', action });
+  ReactGA.event({ category: "button-click", action });
 };
-export const logException = (description = '', fatal = false) => {
+export const logException = (description = "", fatal = false) => {
   if (description) {
     ReactGA.exception({ description, fatal });
   }
